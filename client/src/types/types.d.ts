82a642e6a027eea999c93a4fe87d5b432509568f
Francis Lagares <francis.lagares@gmail.com>
2022-05-11 15:48:12 +0200

    feat: define ITransaction interface

--- client/src/types/types.d.ts
+++ client/src/types/types.d.ts
@@ -1,3 +1,13 @@
 type Click = React.MouseEvent<HTMLInputElement>;
 type Change = React.ChangeEvent<HTMLInputElement>;
 type Submit = React.FormEvent<HTMLButtonElement>;
+
+interface ITransaction {
+  addressTo: string;
+  addressFrom: string;
+  timestamp: string;
+  message: string;
+  keyword: string;
+  amount: string;
+  url: string;
+}
